d33bec1f6a89a9fd648eb04df433f79bbb683ad7
Kumar240 <110481802+Kumar240@users.noreply.github.com>
2024-08-21 14:26:39 +0530

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -1,18 +1,17 @@
 terraform {
   required_version = "~> 1.9.3"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
   }
-terraform {
+
   backend "s3" {
-    bucket = "mybucket2108k"
-    key    = "eks/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "mybucket2108k"
+    key            = "eks/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "Lock-Files"
   }
-}
-provider "aws" {
-  region = "us-east-1"
 }
